b6146e3be60165284bfbfa8e632e0f25b6b9dbe5
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-11-24 01:50:23 +0200

    Update email.c

--- email.c
+++ email.c
@@ -6,14 +6,20 @@ https://www.youtube.com/watch?v=cIkzOOcdh2Y
 */
 #include<stdio.h>
 #include <pthread.h> 
-
+Semaphore *createSem(void *value)
+{
+  Semaphore *sem = check_malloc(sizeof(Semaphore));
+  int n = sem_init(sem, 0, value);
+  if (n != 0) perror_exit("sem_init failed");
+  return sem;
+}
 int main()
 {
 int Nmessages;// count independent incoming messages in a system, 
 int mmonitor_count;//and another thread mmonitor gets the count of threads at time intervals of size t1
 pthread_t threadCreator; 
 printf("Thread Semaphore Creator");
-pthread_create(&threadCreator, NULL, createSem, NULL); 
+pthread_create(&threadCreator, NULL, createSem, (void*)semaphore_Value); 
 
     return 0;
 }
