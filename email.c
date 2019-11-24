449f5a1c0d510b00070fd77f4bba447ca9e0bcf4
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-11-24 17:34:36 +0200

    Update email.c

--- email.c
+++ email.c
@@ -6,20 +6,46 @@ https://www.youtube.com/watch?v=cIkzOOcdh2Y
 */
 #include<stdio.h>
 #include <pthread.h> 
-Semaphore *createSem(void *value)
+#include <semaphore.h> 
+void *createSem(void *value_s);
+
+typedef struct {
+int value, wakeups;
+pthread_mutex_t  *mutex;
+} Semaphore;
+
+void perror_exit (char *s)
 {
-  Semaphore *sem = check_malloc(sizeof(Semaphore));
+  perror (s);  exit (-1);
+}
+
+
+
+void  *createSem(void *value_s)
+
+{printf("In Function createSem");
+    Semaphore SemaphoreCreated;
+int value= *(int *) value_s;
+ printf("Value Of Semaphore%d ",value_s);
+
+  Semaphore *sem = malloc(sizeof(Semaphore));
+/*int sem_init(sem_t *sem, int pshared, unsigned int value);*/
   int n = sem_init(sem, 0, value);
-  if (n != 0) perror_exit("sem_init failed");
-  return sem;
+
+    
+    
 }
 int main()
 {
 int Nmessages;// count independent incoming messages in a system, 
 int mmonitor_count;//and another thread mmonitor gets the count of threads at time intervals of size t1
+int messagesID;
 pthread_t threadCreator; 
+int value =5 ;
+    int *semaphore_Value;
+    semaphore_Value=&value;
+pthread_create(threadCreator, NULL, createSem, (void*)semaphore_Value);
 printf("Thread Semaphore Creator");
-pthread_create(&threadCreator, NULL, createSem, (void*)semaphore_Value); 
 
     return 0;
 }
