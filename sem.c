9e227bd00caa61a8305efacef8b429d75c65d37e
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-12-01 01:38:03 +0200

    Update sem.c

--- sem.c
+++ sem.c
@@ -1,9 +1,11 @@
 /*https://www.sourceware.org/pthreads-win32/manual/pthread_mutex_init.html*/
 #include <stdio.h>
 #include <pthread.h>
-
+#include <stdlib.h>
 #define NTHREADS 10
 void *thread_function(void *);
+void *save_Buffer_thread(void *);
+
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 int  counter = 0;
 
@@ -21,20 +23,35 @@ thread_id = malloc(sizeof(pthread_t)*NTHREADS);
 
    for(j=0; j < NTHREADS; j++)
    {
-      pthread_join( thread_id[j], NULL); 
+      pthread_join( thread_id[j], NULL);
    }
-  
-   /* Now that all threads are complete I can print the final result.     */
-   /* Without the join I could be printing a value before all the threads */
-   /* have been completed.                                                */
 
-   printf("Final counter value: %d\n", counter);
+
+
+
+
+
+int value =counter ;
+    int *p_buffer;
+    p_buffer=&value;
+   //Push to buffer
+       pthread_t reader_Buffer_thread;
+
+         pthread_create(&reader_Buffer_thread, NULL,save_Buffer_thread, p_buffer);
+      pthread_join(reader_Buffer_thread, NULL);
 }
+void *save_Buffer_thread(void *param_buffer)
+{
+      int buffer = *(int *) param_buffer;
+      printf("Buffer - > :%d",buffer);
+}
+
 
 void *thread_function(void *dummyPtr)
 {
    printf("Thread number %ld\n", pthread_self());
    pthread_mutex_lock( &mutex1 );
    counter++;
+
    pthread_mutex_unlock( &mutex1 );
 }
