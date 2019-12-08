ac67e5c60dfda81d596563ebd3994055fa12f307
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-12-09 01:18:35 +0200

    Update sem.c

--- sem.c
+++ sem.c
@@ -3,58 +3,58 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#define NTHREADS 25
+#define NTHREADS 2
 
 pthread_t mCounter;
-
 pthread_t pth_sender;
 pthread_t pth_receiver;
 pthread_mutex_t sem_lock_buffer; 
-
 int count_message; 
 int num_attempts=0;
+int buffer;
 
-
-void *send_message(void* a)
-{
+void *send_message(void*a)
+{    while(1)
+    {
     pthread_mutex_lock(&sem_lock_buffer); // before entering critical section and sending the message and changing the buffer sem-writer
-    printf("message from thread \n");
-    printf("sender write .. \n");
+    printf("Wake Up thread %d \n",count_message);//threadid-> nomattemmp
+    count_message++;
     pthread_mutex_unlock(&sem_lock_buffer);
+      sleep(2);
+
+    }
+  
 }
 
 
 void *receive_message(void* a)
-{
+{while(1){
     pthread_mutex_lock(&sem_lock_buffer);
-    printf("message from thread \n");
-    printf("thread ID  %ld", pthread_self());
+        buffer= count_message;
+
+    printf("\ncounter -> %d ", count_message);
+    printf("BUFFER -> %d",buffer);
+    printf("\nsem wait  .. \n");
+            count_message=0;    
+
     pthread_mutex_unlock(&sem_lock_buffer);
+          sleep(2);
+
+ }
 }
 
 
-void *mCounter_message(void* a)
 
-{
-    
- while(1 && num_attempts!=NTHREADS){
-        pthread_create(&pth_sender,NULL,send_message,NULL);
-        pthread_create(&pth_receiver,NULL,receive_message,NULL);
-        num_attempts+=1;
-
-    }
-
-    pthread_join(pth_sender, NULL);
-    pthread_join(pth_receiver, NULL);   
-    
-    
-}
 int main()
 {
     
-        pthread_create(&mCounter,NULL,mCounter_message,NULL);
-    pthread_join(mCounter, NULL);   
-
+    while(num_attempts!=NTHREADS){
+    /* ->*/   pthread_create(&pth_sender,NULL,send_message,NULL);//attemp
+        pthread_create(&pth_receiver,NULL,receive_message,NULL);
+        num_attempts+=1;
+    }
+    pthread_join(pth_sender, NULL);
+    pthread_join(pth_receiver, NULL);    
     pthread_mutex_destroy(&sem_lock_buffer);
 
     return 0;
